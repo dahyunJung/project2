7ebc07eff62bae12b4a0f9d58a5721884d8bb303
user <user@DESKTOP-COJ9858>
2023-04-28 00:11:22 +0900

    0428 j 에피소드 vo

---
+++ src/main/java/EpisodeVO/User/LookEpisodeVO.java
@@ -0,0 +1,33 @@
+package EpisodeVO.User;
+
+public class LookEpisodeVO {
+
+	private String novelTitle;
+	private String epTitle;
+	private String epDetail;
+	
+	public String getNovelTitle() {
+		return novelTitle;
+	}
+
+	public void setNovelTitle(String novelTitle) {
+		this.novelTitle = novelTitle;
+	}
+
+	public String getEpTitle() {
+		return epTitle;
+	}
+
+	public void setEpTitle(String epTitle) {
+		this.epTitle = epTitle;
+	}
+
+	public String getEpDetail() {
+		return epDetail;
+	}
+
+	public void setEpDetail(String epDetail) {
+		this.epDetail = epDetail;
+	}
+
+}
