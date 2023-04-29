bd896ee3c0c9769ae210e22a5999281a9f5b3228
user <user@DESKTOP-COJ9858>
2023-04-29 15:58:23 +0900

    0429/ety/랭킹수정

--- src/main/java/novel/RankingDAO.java
+++ src/main/java/novel/RankingDAO.java
@@ -29,14 +29,13 @@ public class RankingDAO {
 		try {
 			con=dbCon.getConn();
 			StringBuilder sb=new StringBuilder();
-			sb.append(" SELECT n.num_novel, n.photo, n.age, n.title, m.id, COUNT(l.num_like) AS liken_count,	")
-					.append("       e.visit, ep.num_episode, n.story, n.end,	")
-					.append("       RANK() OVER (ORDER BY e.visit DESC) AS visit_rank,	")
-					.append("       COUNT(ep.num_episode) AS episode_count, MAX(ep.make) AS max_make	")
-					.append("FROM novel n	")
-					.append("JOIN member m ON n.num_member = m.num_member	")
-					.append("LEFT JOIN liken l ON n.num_novel = l.num_novel	")
-					.append("LEFT JOIN (	")
+			sb.append(" SELECT n.num_novel, n.photo, n.age, n.end, n.title, m.id, COUNT(l.num_like) AS liken_count,	")
+					.append("       e.visit, n.story, COUNT(ep.num_episode) AS episode_count, MAX(ep.make) AS max_make,	")
+					.append("       RANK() OVER (ORDER BY e.visit DESC) AS visit_rank	")
+					.append("	FROM novel n	")
+					.append("	JOIN member m ON n.num_member = m.num_member	")
+					.append("	LEFT JOIN liken l ON n.num_novel = l.num_novel	")
+					.append("	LEFT JOIN (	")
 					.append("  SELECT num_novel, MAX(visit) AS visit	")
 					.append("  FROM episode	")
 					.append("  WHERE make >= SYSDATE - ").append(i)
@@ -44,8 +43,8 @@ public class RankingDAO {
 					.append("    ) e ON n.num_novel = e.num_novel	")
 					.append("    LEFT JOIN episode ep ON n.num_novel = ep.num_novel AND e.visit = ep.visit	")
 					.append("    WHERE n.make >= SYSDATE - ").append(i)
-					.append("	AND e.visit IS NOT NULL	")
-					.append("	GROUP BY n.num_novel, n.photo, n.age, n.title, m.id, e.visit, ep.num_episode, n.story, n.end	")
+					.append("	AND e.visit IS NOT NULL and n.open=1	")
+					.append("	GROUP BY n.num_novel, n.photo, n.age, n.title, m.id, e.visit, n.story, n.end	")
 					.append("	ORDER BY e.visit DESC ");
 				
 			pstmt=con.prepareStatement(sb.toString());
